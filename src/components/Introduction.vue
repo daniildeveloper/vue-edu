0e359cf32291de9f35984565f68554fe8ace0d17
daniiltserin <daniilborowkow@yandex.ru>
2017-09-02 13:23:55 +0600

    Itreacting with users input

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,10 +1,25 @@
 <template>
   <div id="message">
     {{message}}
+    <hr>
+
+
     <p v-if="seen">Now you see me</p>
+    <hr>
+
+
     <ul>
       <li v-for="todo in todos" v-bind:key="todo.text"> {{todo.text}} </li>
     </ul>
+    <hr>
+
+    <div class="message-reverse">
+      <p>{{message}}</p>
+      <button v-on:click="reverseMessage">Reverse</button>
+    </div>
+    <hr>
+
+    
   </div>
 </template>
 
@@ -28,6 +43,12 @@ export default {
           text: 'Build something awesome'
         }
       ]
+    }
+  },
+
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
     }
   }
 }
