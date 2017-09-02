c25f048124f4245a27e45a629a6123bca601436d
daniiltserin <daniilborowkow@yandex.ru>
2017-09-02 15:00:59 +0600

    Class Binding

---
+++ src/components/Binding.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="binding">
+    <div v-bind:class="{active: isActive}">isActive</div>
+    <div id="inlineStylesBinding" v-bind:style="{color: activeColor, fontSize: fontSize + 'px' }">
+        Inline Style
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Binding',
+  data () {
+    return {
+      isActive: true,
+      activeColor: 'red',
+      fontSize: 30
+    }
+  }
+}
+</script>
+
+<style scoped >
+.active {
+  color: green;
+}
+</style>
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Introduction from '@/components/Introduction'
+import Binding from '@/components/Binding'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/hello',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/binding',
+      name: 'Binding',
+      component: Binding
     }
   ]
 })
